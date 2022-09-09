f0c79b19edb99d60965eeaf793ec1a3b36ce131d
gacalaza <gacalaza@student.42sp.org.br>
2022-09-09 22:41:27 +0200

    memmove funcionando e passando nos testes

--- test_Libft/ft_memmove.c
+++ test_Libft/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/08 01:32:45 by coder             #+#    #+#             */
-/*   Updated: 2022/09/09 20:30:32 by coder            ###   ########.fr       */
+/*   Updated: 2022/09/09 22:39:38 by coder            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,8 @@ void	*ft_memmove(void	*dest, const void	*src, size_t len)
 {
 	char	*strdest;
 	char	*strsrc;
-	size_t		count;
+	size_t	count;
+
 	strdest = (char *)dest;
 	strsrc = (char *)src;
 	count = 0;
@@ -27,14 +28,15 @@ void	*ft_memmove(void	*dest, const void	*src, size_t len)
 		while (len--)
 		{
 			*(strdest + len) = *(strsrc + len);
-			return (dest);
 		}
 	}
 	else
-	while (count < len)
 	{
+		while (count < len)
+		{
 			*(strdest + count) = *(strsrc + count);
 			count++;
+		}
 	}
 	return (dest);
 }
