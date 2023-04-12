71dc45af674976bc15882a056ff349c246101315
gacalaza <gacalaza@student.42.fr>
2023-04-12 01:36:56 -0300

    new tests. NOt even close.

--- minitalk_test/test_client.c
+++ minitalk_test/test_client.c
@@ -6,54 +6,69 @@
 /*   By: gacalaza <gacalaza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/10 18:52:56 by gacalaza          #+#    #+#             */
-/*   Updated: 2023/04/10 19:48:29 by gacalaza         ###   ########.fr       */
+/*   Updated: 2023/04/12 01:23:49 by gacalaza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "include/minitalk.h"
 
-static void	decoder(unsigned char ch, pid_t pid)
+static void	coder(unsigned char ch, pid_t pid)
 {
 	int				c;
 	unsigned int	base;
 
-	c = 7;
-	base = 128;
-	while (c >= 0)
+	c = 7; // contador para os bits da mensagem
+	base = 128; // 1000 0000 em binário, base para calcular o valor de cada bit
+	while (c >= 0) // iterando pelos bits da mensagem
 	{
-		if (ch < base)
+		if (ch < base) // se o bit atual é zero
 		{
-			ft_putchar_fd('0', 1);
-			kill(pid, SIGUSR1);
+			// ft_putchar_fd('0', 1); // escreve 0 no console
+			kill(pid, SIGUSR1); // envia sinal SIGUSR1 para o processo receptor
 		}
-		else
+		else // se o bit atual é um
 		{
-			ft_putchar_fd('1', 1);
-			kill(pid, SIGUSR2);
-			ch = ch - base;
+			// ft_putchar_fd('1', 1); // escreve 1 no console
+			kill(pid, SIGUSR2);  // envia sinal SIGUSR2 para o processo receptor
+			ch = ch - base; // calcula o valor do bit atual
 		}
-		base = base / 2;
-		c--;
-		usleep(200);
+		base = base / 2; // divide a base por 2 para calcular o próximo bit
+		c--; // decrementa o contador para processar o próximo bit
+		usleep(200);// espera 200 microssegundos antes de enviar o próximo sinal
 	}
 }
 
-int	main(int args, char **argv)
+static void	roger_that(int sig)
 {
-	int	counter;
-	int	server_pid;
+	if (sig == SIGUSR1)
+	{
+		ft_putstr_fd("Message received.\n", 1);
+	}
+}
 
-	counter = 0;
-	if (args == 3)
+int	main(int argc, char *argv[])
+{
+	int					counter;
+	int					server_pid;
+	struct sigaction	sa;
+
+	// Configurar o manipulador de sinal para SIGUSR1
+	sa.sa_handler = roger_that;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
+	sigaction(SIGUSR1, &sa, NULL);
+	counter = 0; // contador para os caracteres da mensagem
+	if (argc == 3)
 	{
 		server_pid = ft_atoi(argv[1]);
 		while (argv[2][counter])
 		{
-			decoder(argv[2][counter], server_pid);
-			ft_putchar_fd('\n', 1);
+			coder(argv[2][counter], server_pid); // convertendo cada caractere em uma sequência de bits e enviando para o processo receptor
+			// ft_putchar_fd('\n', 1);
 			counter++;
 		}
-		decoder('\n', server_pid);
+		coder('\n', server_pid);
+		coder('\0', server_pid);
 	}
 	return (0);
 }
--- minitalk_test/tests_server.c
+++ minitalk_test/tests_server.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   tests.c                                            :+:      :+:    :+:   */
+/*   tests_server.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gacalaza <gacalaza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/10 16:13:01 by gacalaza          #+#    #+#             */
-/*   Updated: 2023/04/10 20:13:47 by gacalaza         ###   ########.fr       */
+/*   Updated: 2023/04/12 01:19:48 by gacalaza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,31 +14,91 @@
 
 static void	decoder(int sig, siginfo_t *info, void *context)
 {
-	static int	counter = 0;
-	static char	ch = 0;
+	static int	count = 0;
+	static char	c = 0;
 	(void)info;
+	(void)context;
 
 	if (sig == SIGUSR2)
-		ch = ch | 128 >> counter;
-	counter++;
-	if (counter == 8)
+		c = c | 128 >> count; // setar o MSB em c qndo client envia um sinal 
+		//SIGUSR2, o bit "1" é adicionado em c no bit mais significativo 
+		// (128 >> counter) e essa operação é realizada até q os 8 bits q compõem 
+		// o char estejam completos
+	count++;
+	// Após o recebimento de um bit, a var count é incrementada e é 
+	// verificado se foram recebidos os 8 bits de um char. 
+	// Quando count atinge 8, é impresso o caractere correspondente usando 
+	// ft_putchar_fd e as variáveis count e c são resetadas
+	if (count == 8)
 	{
-		ft_putchar_fd(ch, 1);
-		ch = 0;
-		counter = 0;
+		ft_putchar_fd(c, 1);
+		c = 0;
+		count = 0;
 	}
 }
+
+// static void decoder(int sig, siginfo_t *info, void *context)
+// {
+// 	static int count = 0;
+// 	static unsigned char buffer = 0;
+// 	(void)context;
+
+// 	if (sig == SIGUSR2)
+// 		buffer |= (1 << (7 - count));
+// 	count++;
+
+// 	if (count == 8)
+// 	{
+// 		ft_putchar_fd(buffer, 1);
+// 		count = 0;
+// 		buffer = 0;
+// 	}
+// 	// Verifica se a mensagem foi recebida completa
+// 	if (buffer == '\0')
+// 	{
+// 		// Envia sinal de confirmação ao cliente
+// 		kill(info->si_pid, SIGUSR2);
+// 	}
+// }
+
+
+// static void	decoder(int sig, siginfo_t *info, void *context)
+// {
+// 	static unsigned char	byte = 0;
+// 	static int				bit_count = 0;
+// 	unsigned char			mask;
+
+// 	(void)context;
+// 	mask = 1 << bit_count;
+// 	if (sig == SIGUSR1)
+// 		byte &= ~mask;
+// 	else if (sig == SIGUSR2)
+// 		byte |= mask;
+
+// 	bit_count++;
+// 	if (bit_count == 8)
+// 	{
+// 		if (byte == '\0')
+// 			kill(info->si_pid, SIGUSR2); // envia sinal de volta para o cliente
+// 		else
+// 			ft_putchar_fd(byte, 1);
+// 		byte = 0;
+// 		bit_count = 0;
+// 	}
+// }
 
 int	main(void)
 {
 	struct sigaction	sa1;
 	struct sigaction	sa2;
-	int					my_pid;
+	pid_t				my_pid;
 	
 	my_pid = getpid();
-	ft_printf("Server PID is %d", my_pid);
+	ft_printf("Server PID is %d", (int)my_pid);
 	ft_putchar_fd('\n', 1);
-	sa1.sa_flags = SA_SIGINFO;
+	sa1.sa_flags = SA_SIGINFO; //A flag SA_SIGINFO não afeta o comportamento do 
+	// manipulador de sinal(decoder), apenas define que informações adicionais sobre o 
+	// sinal serão passadas para o manipulador. mas devo inicializar os membros da struct
 	sa1.sa_sigaction = decoder;
 	sigaction(SIGUSR1, &sa1, NULL);
 	sa2.sa_flags = SA_SIGINFO;
