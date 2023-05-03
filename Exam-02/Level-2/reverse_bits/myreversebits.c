a7153622aa271fe3a22463726468bfac616077d6
gacalaza <gacalaza@student.42.fr>
2023-05-03 11:47:23 -0300

    The true sign of wisdom is acknowledging the vastness of the unknown. The more we learn, the more we realize the immensity of what is still undiscovered. It is in the humility before the unknown that we find true wisdom.

--- Exam-02/Level-2/reverse_bits/myreversebits.c
+++ Exam-02/Level-2/reverse_bits/myreversebits.c
@@ -6,7 +6,7 @@
 /*   By: gacalaza <gacalaza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/01 16:11:41 by gacalaza          #+#    #+#             */
-/*   Updated: 2023/05/01 20:05:09 by gacalaza         ###   ########.fr       */
+/*   Updated: 2023/05/03 11:43:54 by gacalaza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,55 +14,42 @@
 
 unsigned char	reverse_bits(unsigned char octet)
 {
-	// int				i = 7;
-	// unsigned char	temp = 0;
-	// unsigned char	temp2 = 0;
+	int				i = 8;
+	unsigned char	result = 0;
 
-	octet = (octet & 0xF0) >> 4 | (octet & 0xF0) << 4;
-	octet = (octet & 0xCC) >> 2 | (octet & 0xCC) << 2;
-	octet = (octet & 0xAA) >> 1 | (octet & 0xAA) << 1;
-
-	return (octet);
-	// while (i >= 0)
-	// {
-	// 	if((128 << i) | octet)
-	// 		temp = 128 << i;
-	// 	i--;
-	// }
-	// i = 0;
-	// 	while (i < 7)
-	// {
-	// 	if((128 >> i) | octet)
-	// 		temp2 = 128 >> i | temp;
-	// 	i++;
-	// }
-	// return (temp2);
-}
-
-void	printBinary(unsigned char c)
-{
-	int i = 0;
-
-	while (i < 8)
+	while (i > 0)
 	{
-		if ((128 >> i) & c)
-			printf("1");
-		else
-			printf("0");
-		i++;
+		result = result * 2 + octet % 2;
+		octet /= 2;
+		i--;
 	}
-	if (i == 8)
-		printf("\n");
+	return (result);
 }
 
+// void	printBinary(unsigned char c)
+// {
+// 	int i = 0;
 
-int	main()
-{
-	char character = '3';
-	printf("Valor binário do caractere '%c': \n", character);
-	printBinary(character);
+// 	while (i < 8)
+// 	{
+// 		if ((128 >> i) & c)
+// 			printf("1");
+// 		else
+// 			printf("0");
+// 		i++;
+// 	}
+// 	if (i == 8)
+// 		printf("\n");
+// }
 
-	printBinary(reverse_bits(character));
 
-	return (0);
-}
+// int	main()
+// {
+// 	char character = '3';
+// 	printf("Valor binário do caractere '%c': \n", character);
+// 	printBinary(character);
+
+// 	printBinary(reverse_bits(character));
+
+// 	return (0);
+// }
