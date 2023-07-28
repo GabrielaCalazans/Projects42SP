58b9a85784449bd8a9762fbc93f85c9725eb3049
gacalaza <gacalaza@42sp.org.br>
2023-07-28 15:48:54 -0300

    new tests.

--- Exams/Exam-02/Level-2/reverse_bits/myreversebits.c
+++ Exams/Exam-02/Level-2/reverse_bits/myreversebits.c
@@ -6,11 +6,27 @@
 /*   By: gacalaza <gacalaza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/01 16:11:41 by gacalaza          #+#    #+#             */
-/*   Updated: 2023/05/03 11:43:54 by gacalaza         ###   ########.fr       */
+/*   Updated: 2023/07/27 18:19:43 by gacalaza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+
+void	printBinary(unsigned char c)
+{
+	int i = 0;
+
+	while (i < 8)
+	{
+		if ((128 >> i) & c)
+			printf("1");
+		else
+			printf("0");
+		i++;
+	}
+	if (i == 8)
+		printf("\n");
+}
 
 unsigned char	reverse_bits(unsigned char octet)
 {
@@ -19,37 +35,52 @@ unsigned char	reverse_bits(unsigned char octet)
 
 	while (i > 0)
 	{
+		printf("func result: %c':", result);
+		printBinary(result);
 		result = result * 2 + octet % 2;
+		printf("func octet: %c':", octet);
+		printBinary(octet);
+		printf("\n");
 		octet /= 2;
 		i--;
 	}
 	return (result);
 }
 
-// void	printBinary(unsigned char c)
-// {
-// 	int i = 0;
+unsigned char	reverse_bits_bitwise(unsigned char octet)
+{
+	int				i = 7;
+	unsigned char	result = 0;
 
-// 	while (i < 8)
-// 	{
-// 		if ((128 >> i) & c)
-// 			printf("1");
-// 		else
-// 			printf("0");
-// 		i++;
-// 	}
-// 	if (i == 8)
-// 		printf("\n");
-// }
+	printf("func-bitwise RESULT: %c\n", result);
+	printf("func-bitwise OCTET: %c\n", octet);
+	while (i >= 0)
+	{
+		printf("func-bitwise RESULT:");
+		printBinary(result);
 
+		result = result | (octet & 1) << i;
 
-// int	main()
-// {
-// 	char character = '3';
-// 	printf("Valor binário do caractere '%c': \n", character);
-// 	printBinary(character);
+		printf("func-bitwise OCTET:");
+		printBinary(octet);
+		printf("\n");
 
-// 	printBinary(reverse_bits(character));
+		octet = octet >> 1;
+		i--;
+	}
+	return (result);
+}
 
-// 	return (0);
-// }
+int	main()
+{
+	char character = 'A';
+	printf("Valor binário do caractere ANTES-MAIN: %c':", character);
+	printBinary(character);
+	printf("Valor binário do caractere DEPOIS DA FUNC REV-MAIN");
+	printBinary(reverse_bits(character));
+	printf("\n");
+	printf("Valor binário do caractere DEPOIS DA FUNC REV_bitwise-MAIN");
+	printBinary(reverse_bits_bitwise(character));
+
+	return (0);
+}
--- testes_aleatorios/gen_nbrs.sh
+++ testes_aleatorios/gen_nbrs.sh
@@ -4,3 +4,7 @@ ARG=$(seq 2000 | shuf -n 50); ./push_swap $ARG
 ARG=$(seq 2000 | shuf -n 100); ./insertion_sort $ARG
 
 ARG=$(seq 2000 | shuf -n 1000); ./insertion_sort $ARG
+
+ARG=$(seq -50 49 | shuf); ./push_swap $ARG | ./checker_linux
+
+ARG=$(seq -100 99 | shuf); ./push_swap $ARG
