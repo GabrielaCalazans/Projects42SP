414fa271ea5eab8f3a8e15e5518f0c8349e3512a
gacalaza <gacalaza@42sp.org.br>
2023-09-15 20:28:13 -0300

    adding tests.

--- minishell_tests/mandatory/minishell.c
+++ minishell_tests/mandatory/minishell.c
@@ -3,16 +3,40 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gacalaza <gacalaza@student.42.fr>          +#+  +:+       +#+        */
+/*   By: gacalaza <gacalaza@student.42sp.org.br     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/24 17:36:31 by gacalaza          #+#    #+#             */
-/*   Updated: 2023/08/28 21:01:07 by gacalaza         ###   ########.fr       */
+/*   Updated: 2023/09/15 20:17:30 by gacalaza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-int	main(int argc, char *argv[], char* envp[])
+int main(int argc, char *argv[], char* envp[])
 {
+	char *input;
 	
+	(void)argv;
+	(void)envp;
+	if (argc != 1)
+		return (1);
+	while (1)
+	{
+		input = readline("chora$ ");
+		if (input == NULL)
+		{
+			printf("\n");
+			break;
+		}
+		if (input[0] != '\0')
+		{
+			// Adiciona o comando à história se não estiver vazio
+			add_history(input);
+			// Agora 'input' contém o comando digitado pelo usuário
+			printf("That's what she said: %s\n", input);
+		}
+		free(input);
+		input = NULL;
+	}
+	return 0;
 }
---
+++ minishell_tests/pai.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pai.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gacalaza <gacalaza@student.42sp.org.br     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/14 16:18:00 by gacalaza          #+#    #+#             */
+/*   Updated: 2023/09/15 19:51:15 by gacalaza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "./include/minishell.h"
+
+int main()
+{
+	char	*program_path;
+	char	*env_var; // variável de ambiente que quiser acessar
+	char	*env_value;
+	char	*command[2];
+	int		status;
+	pid_t	pid;
+	
+	env_var = "USER";
+	program_path = "./filho";
+	command[0] = program_path;
+	command[1] = NULL;
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("fork");
+		return 1;
+	}
+	if (pid == 0)
+	{
+		env_value = getenv(env_var); // Usando getenv para obter o valor da variável de ambiente
+		if (env_value == NULL)
+		{
+			perror("getenv");
+			return 1;
+		}
+		// Este é o processo filho
+		printf("env_value: %s\n", env_value);
+		execve(program_path, command, &env_var);
+		// Se chegarmos aqui, algo deu errado com execve
+		perror("execve");
+		exit(1);
+	}
+	else
+	{
+		if (pid != 0) // Verifica se este é o processo pai
+		{
+			printf("Executando o programa pai...\n");
+		}
+		waitpid(pid, &status, 0);
+		printf("-Execução do programa filho concluída.\n");
+	}
+	return 0;
+}
