586e4bde9a96b068e0940c730dd91739b72b8f97
gacalaza <gacalaza@42sp.org.br>
2023-09-18 15:00:09 -0300

    nothing

--- minishell_tests/mandatory/prompt.c
+++ minishell_tests/mandatory/prompt.c
@@ -6,7 +6,7 @@
 /*   By: gacalaza <gacalaza@student.42sp.org.br     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/16 15:55:06 by gacalaza          #+#    #+#             */
-/*   Updated: 2023/09/16 21:39:34 by gacalaza         ###   ########.fr       */
+/*   Updated: 2023/09/18 14:52:01 by gacalaza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	prompt(void)
 {
 	t_init	*tmp;
 
-	tmp = (t_init *)malloc(sizeof(t_init)); // Aloca a estrutura t_init
+	tmp = (t_init *)malloc(sizeof(t_init));
 		if (tmp == NULL)
 		{
 			perror("Memory allocation failure at prompt func.");
@@ -36,7 +36,7 @@ void	prompt(void)
 			// Adiciona o comando à história se não estiver vazio
 			add_history(tmp->prompt_input);
 			// Agora 'input' contém o comando digitado pelo usuário
-			printf("That's what she said: %s\n", tmp->prompt_input);
+			printf("That's what he said: %s\n", tmp->prompt_input);
 		}
 		free(tmp->prompt_input);
 		tmp->prompt_input = NULL;
