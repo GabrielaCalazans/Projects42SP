ddc79181bf4169816c471bebcf7613681fd3d151
gacalaza <gacalaza@student.42.fr>
2023-11-07 23:13:55 -0300

    starting to get the words to parse.

--- minishell_tests/mandatory/parse/parse_utils.c
+++ minishell_tests/mandatory/parse/parse_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parse_utils.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gacalaza <gacalaza@student.42sp.org.br     +#+  +:+       +#+        */
+/*   By: gacalaza <gacalaza@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 21:14:38 by gacalaza          #+#    #+#             */
-/*   Updated: 2023/11/04 19:06:16 by gacalaza         ###   ########.fr       */
+/*   Updated: 2023/11/07 23:12:42 by gacalaza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int	has_d_redirec_p(t_token *tokens)
 	r_rdrcts = 0;
 	while (temp)
 	{
-		if (temp->type == HEREDOC || temp->type == AMPERSAND)
+		if (temp->type == HEREDOC || temp->type == APPEND)
 			r_rdrcts++;
 		if (temp->type == PIPE)
 			break ;
@@ -29,3 +29,34 @@ int	has_d_redirec_p(t_token *tokens)
 	}
 	return (r_rdrcts);
 }
+
+int	nb_words(t_token *tokens)
+{
+	t_token	*temp;
+	int		words;
+
+	temp = tokens;
+	words = 0;
+	while (temp)
+	{
+		if (temp->type == WORD)
+			words++;
+		if (temp->type == PIPE)
+			break ;
+		temp = temp->next;
+	}
+	return (words);
+}
+
+// char	**get_words(t_token *tokens)
+// {
+// 	t_token	*temp;
+// 	int		nb_words;
+
+// 	temp = tokens;
+// 	nb_words = 0;
+// 	while (temp)
+// 	{
+
+// 	}
+// }
